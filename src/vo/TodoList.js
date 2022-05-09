a8cfac6ce439754ee67688c297710884f4d3756f
Yesung(Isaac) Lee <49810053+Isaac-Lee@users.noreply.github.com>
2022-05-09 19:05:44 +0900

    Update TodoList.js

--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -50,7 +50,8 @@ class TodoList {
 
   get equalsDayAndNotCompletedItems() {
     return this._items.filter(this._equalsDayAndNotCompletedFilter)
-
+  }
+  
   get items() {
     return this._items
   }
